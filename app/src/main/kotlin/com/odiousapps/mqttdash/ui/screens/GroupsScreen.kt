b26017e3ed825ea6f46994b56fa3e2c0b48d76b0
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-08-23 12:10:58 +1000

    flush

--- app/src/main/kotlin/com/odiousapps/mqttdash/ui/screens/GroupsScreen.kt
+++ app/src/main/kotlin/com/odiousapps/mqttdash/ui/screens/GroupsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
@@ -33,6 +34,7 @@ import androidx.navigation.NavController
 import com.odiousapps.mqttdash.MqttDashApplication
 import com.odiousapps.mqttdash.data.PanelGroup
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GroupsScreen(navController: NavController) {
     val app = LocalContext.current.applicationContext as MqttDashApplication
