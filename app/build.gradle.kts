be3c34129320fd6815ab3ccaa4ecc59ac66bda14
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-08-29 03:20:46 +1000

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.odiousapps.z2mdash"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "0.0.9"
+        versionCode = 10
+        versionName = "0.0.10"
     }
 
     buildTypes {
